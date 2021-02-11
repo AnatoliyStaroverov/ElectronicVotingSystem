d0a49880f1353ba60eece3cb3dc3b2f3e2894658
astarove <anatoliy.staroverov1234@gmail.com>
2021-02-10 16:32:07 -0800

    added frontend stuff.

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,104 @@
-import React ,{Component} from 'react';
-import { BrowserRouter,Route, Switch } from 'react-router-dom';
-import HomePage from './views/HomePage';
-import AuthHomePage from './views/AuthHomePage';
-import VotingPage from './views/VotingPage';
+import './assets/tailwind.css'; 
 
-export default class App extends Component {
-  static displayName = App.name;
+import React from 'react'; 
 
-  render () {
-    return (
-      <BrowserRouter>
-      <Switch>
-        <Route path='/' component={HomePage} exact />
-        <Route path='/AuthHomePage' component={AuthHomePage}  />
-        <Route path='/VotingPage' Component={VotingPage} />
-      </Switch>
-      </BrowserRouter>
+import Home from './components/Home'; 
+import Login from './components/Login'; 
+import Candidates from './components/Candidates'; 
+import CreateAccount from './components/CreateAccount'; 
+import Map from './components/Map'; 
+import VotingBallot from './components/VotingBallot'; 
+import Logout from './components/Logout'; 
+import Timer from './components/Timer'; 
+import MenuItems from './components/MenuItems'; 
 
+
+
+export default class App extends React.Component
+{
+  constructor(props) {
+    super(props);
+
+    // Global app state.
+    this.state = {
+      username: '',
+      page: 'home',
+    }
+
+    this.onNavSelect = this.onNavSelect.bind(this);
+    this.doStateChange = this.doStateChange.bind(this);
+  }
+
+  // This function is called when someone clicks on a button in the navbar.
+  onNavSelect(page) {
+    this.setState({page});
+  }
+
+  doStateChange(name, value) {
+    if (name === 'page') {
+      this.onNavSelect(value);
+      return;
+    }
+
+    this.setState({[name]: value});
+  }
+  render(){
+    const pages = {
+      'The Candidates': (
+        <Candidates
+          data-testid = "page-candidates"
+        ></Candidates>
+      ),
+      'Electoral Map': (
+        <Map
+          data-testid="page-map"
+        ></Map>
+      ),
+      'Create Account': (
+        <CreateAccount
+          data-testid="page-createAccount"
+          doStateChange={this.doStateChange}
+        >
+        </CreateAccount>
+      ),
+      'Login': (
+        <Login onNavSelect={this.onNavSelect}
+          data-testid="page-login"
+          doStateChange={this.doStateChange}
+        ></Login>
+      ),
+      
+      'votingBallot': (
+        <VotingBallot 
+          data-testid="page-votingBallot"
+          username={this.state.username}>
+        </VotingBallot>
+      ),
+    }
+
+    if (this.state.username) {
+      delete pages['login'];
+      delete pages['createAccount'];
+      pages['logout'] = (<Logout onNavSelect={this.onNavSelect} doStateChange={this.doStateChange}></Logout>)
+    } else {
+      delete pages['votingBallot'];
+    }
+  
+    return(
+      <main>
+        <Home></Home>
+        <MenuItems
+          items={Object.keys(pages)}
+          active={this.state.page}
+          onNavSelect={this.onNavSelect}
+        ></MenuItems>
+        
+  
+        {pages[this.state.page]}
+        
+      </main>
     );
   }
+ 
+
 }
---
+++ client/src/components/Map.jsx
@@ -0,0 +1,16 @@
+import React from 'react'; 
+//import Logo from "../images/HeaderBackground_Blue.jpg"; 
+
+
+
+export default class Map extends React.Component 
+{
+    render(){
+        return (
+            <div>
+            {/* <img className="backgroundImage" src={Logo}></img> */}
+            </div>
+
+        );
+    }
+}
