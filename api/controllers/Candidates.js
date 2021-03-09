8842922f8212ce6d9b061666a7b22cff38a3cb3b
JinyangRuan <jinyang.ruan@wsu.edu>
2021-03-08 22:16:26 -0800

    add route

---
+++ api/controllers/Candidates.js
@@ -0,0 +1,11 @@
+const Candidates = require('../models/Candidates');
+
+
+// post candidates data to database.
+exports.postCandidates = async (req, res) => {
+    const { firstname, lastname, Gender, Age, State, Party} = req.body;
+    const newCandidates = new Candidates({ firstname: firstname, lastname: lastname, Gender: Gender, , Age: Age, State: State, Party: Party});
+    newCandidates.save();
+    console.log("Added Candidates to database ");
+    res.status(200).redirect('http://localhost:3000/');
+}
--- api/server.js
+++ api/server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const cors = require('cors');
 require('dotenv').config();
 const registerRoute = require('./routes/Register');
+const candidateRoute = require('./routes/Candidates');
+const { default: Candidates } = require('../client/src/components/Candidates');
 const app = express();
 
 app.use(cors());
@@ -16,6 +18,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/static', express.static(path.join(`${__dirname}/public`)));
 
 app.use('/register', registerRoute);
+
+app.use('/candidate', candidateRoute);
 
 const port = process.env.PORT || 8080;
 
