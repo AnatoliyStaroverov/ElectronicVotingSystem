9919fac7042f0b8a0c57ae4f772a588368700383
JinyangRuan <jinyang.ruan@wsu.edu>
2021-03-08 22:30:41 -0800

    add route

--- api/controllers/Candidates.js
+++ api/controllers/Candidates.js
@@ -4,7 +4,7 @@ const Candidates = require('../models/Candidates');
 // post candidates data to database.
 exports.postCandidates = async (req, res) => {
     const { firstname, lastname, Gender, Age, State, Party} = req.body;
-    const newCandidates = new Candidates({ firstname: firstname, lastname: lastname, Gender: Gender, , Age: Age, State: State, Party: Party});
+    const newCandidates = new Candidates({ firstname: firstname, lastname: lastname, Gender: Gender, Age: Age, State: State, Party: Party});
     newCandidates.save();
     console.log("Added Candidates to database ");
     res.status(200).redirect('http://localhost:3000/');
--- api/models/Candidate.js
+++ api/models/Candidate.js
@@ -1,1 +1,31 @@
-// JavaScript source code
+const mongoose = require('mongoose');
+
+const CandidateSchema = mongoose.Schema({
+
+    firstname: {
+        type: String,
+        required: true,
+    },
+    lastname: {
+        type: String,
+        required: true,
+    },
+    Gender: {
+        type: String,
+        required: true,
+    },
+    Age: {
+        type: Number,
+        required: true,
+    },
+    State: {
+        type: String,
+        required: true,
+    },
+    Party: {
+        type: String,
+        required: true,
+    },
+});
+
+module.exports = mongoose.model('series', CandidateSchema);
